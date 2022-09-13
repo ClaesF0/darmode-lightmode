52fba44b28cbf9aa3e9654bd72240225982beb9b
ClaesF0 <claes.folkestad@stud.noroff.no>
2022-09-13 13:44:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const checkbox = document.getElementById('checkbox');
+let darkMode = localStorage.getItem("dark-mode")
 
+function enableDarkMode(){
 checkbox.addEventListener('change', () => {
     //change the theme of the website
     document.body.classList.toggle('dark');
+    localStorage.setItem("dark-mode", "enabled");
 });
+}
+
+if (darkMode === "enabled") {
+    enableDarkMode();
+}
