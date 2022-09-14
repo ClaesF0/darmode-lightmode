c12d018fe88c743995403337ed0e14ad39b19dfb
ClaesF0 <claes.folkestad@stud.noroff.no>
2022-09-14 15:22:33 +0200

    .

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const checkbox = document.getElementById('checkbox');
-let darkMode = localStorage.getItem("dark-mode")
 
-function enableDarkMode(){
+// localStorage.setItem
 checkbox.addEventListener('change', () => {
     //change the theme of the website
     document.body.classList.toggle('dark');
-    localStorage.setItem("dark-mode", "enabled");
+    localStorage.setItem("dark-mode", "enabled")
 });
+
+let darkMode = localStorage.getItem("dark-mode");
+
+const disableDarkMode = () => {
+    document.body.classList.toggle('dark');
+    localStorage.setItem("dark-mode", "disabled");
 }
 
-if (darkMode === "enabled") {
-    enableDarkMode();
-}
+
+// localStorage.getItem
